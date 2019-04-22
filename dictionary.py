3640d2aa23e9187425132adef7bf7f2563e40cd2
Abhishek Kalita <abhishek.kalita@vanderbilt.edu>
2019-04-22 03:59:06 -0500

    added word search

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,13 @@
 import json
 
+
+def translate(word):
+    return words[word]
+
+
+
+
 words = json.load(open("data.json"))
 userreq = input("Enter word to get definition: ")
+
+print(translate(userreq))
