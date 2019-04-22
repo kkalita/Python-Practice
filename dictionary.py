81b51e0308620785eedfa1c40f30cfca748ff28b
Abhishek Kalita <abhishek.kalita@vanderbilt.edu>
2019-04-22 04:26:38 -0500

    Revert "added word matching"
    This reverts commit 3a4b40cb670a76a421a3db3a782190c5bc2f02dd.

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,4 @@
 import json
-from difflib import get_close_matches
-
-words = json.load(open("data.json"))
 
 def translate(word):
 
@@ -9,21 +6,11 @@ def translate(word):
     
     if word in words:
         return words[word]
-    #elif get_close_matches(word, words.keys()):
-    #    closest = get_close_matches(word, words.keys())[0]
-    #    confirm = input("Did you mean %s? Y/N: " % closest)
-
-    #    if confirm.lower == "y":
-    #        return closest
-
     else: 
         return "The word doesn't exist - double check it"
 
-def main():
-    
-    userreq = input("Enter word to get definition: ")
-    print(translate(userreq))
+words = json.load(open("data.json"))
+userreq = input("Enter word to get definition: ")
 
-if __name__ == "__main__":
-    main()
+print(translate(userreq))
 
