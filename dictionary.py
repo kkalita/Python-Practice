4d18f603566c1d0b1fac2213225e6ca5c9c34f37
Abhishek Kalita <abhishek.kalita@vanderbilt.edu>
2019-04-22 04:07:27 -0500

    added case sensitivity

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,16 @@
 import json
 
-
 def translate(word):
-    return words[word]
 
-
-
+    word = word.lower()
+    
+    if word in words:
+        return words[word]
+    else: 
+        return "The word doesn't exist - double check it"
 
 words = json.load(open("data.json"))
 userreq = input("Enter word to get definition: ")
 
 print(translate(userreq))
+
