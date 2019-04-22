3a4b40cb670a76a421a3db3a782190c5bc2f02dd
Abhishek Kalita <abhishek.kalita@vanderbilt.edu>
2019-04-22 04:26:18 -0500

    added word matching

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,7 @@
 import json
+from difflib import get_close_matches
+
+words = json.load(open("data.json"))
 
 def translate(word):
 
@@ -6,11 +9,21 @@ def translate(word):
     
     if word in words:
         return words[word]
+    #elif get_close_matches(word, words.keys()):
+    #    closest = get_close_matches(word, words.keys())[0]
+    #    confirm = input("Did you mean %s? Y/N: " % closest)
+
+    #    if confirm.lower == "y":
+    #        return closest
+
     else: 
         return "The word doesn't exist - double check it"
 
-words = json.load(open("data.json"))
-userreq = input("Enter word to get definition: ")
+def main():
+    
+    userreq = input("Enter word to get definition: ")
+    print(translate(userreq))
 
-print(translate(userreq))
+if __name__ == "__main__":
+    main()
 
