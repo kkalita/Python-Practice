59de3678da913b5776120b6db693820f9926db0f
Abhishek Kalita <abhishek.kalita@vanderbilt.edu>
2019-04-22 04:33:34 -0500

    added word matching 2

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,7 @@
 import json
+from difflib import get_close_matches
+
+words = json.load(open("data.json"))
 
 def translate(word):
 
@@ -6,11 +9,19 @@ def translate(word):
     
     if word in words:
         return words[word]
+    elif get_close_matches(word, words.keys()):
+        yn = input("Did you mean %s? Enter Y/N: " % get_close_matches(word, words.keys())[0])
+
+        if yn.lower() == "y":
+            return words[get_close_matches(word, words.keys())[0]]
+
     else: 
         return "The word doesn't exist - double check it"
 
-words = json.load(open("data.json"))
-userreq = input("Enter word to get definition: ")
+def main():
+    userreq = input("Enter word to get definition: ")
 
-print(translate(userreq))
+    print(translate(userreq))
 
+if __name__ == "__main__":
+    main()
