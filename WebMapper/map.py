12a5a70140f348b426a64f58d2f456e4152cdc32
Abhishek Kalita <abhishek.kalita@vanderbilt.edu>
2019-04-23 20:58:40 -0500

    added country population colors

--- WebMapper/map.py
+++ WebMapper/map.py
@@ -1,8 +1,15 @@
 import folium
 import pandas
 
-def main():
+def colorCat(n):
+    if n < 1000: 
+        return 'green'
+    elif n < 2000: 
+        return 'orange'
+    else:
+        return 'red'
 
+def main():
     mapdata = pandas.read_csv("Volcanoes.txt")
     lat = list(mapdata["LAT"])
     lon = list(mapdata["LON"])
@@ -12,8 +19,13 @@ def main():
     fg = folium.FeatureGroup(name = "demoMap")
 
     for lt, ln, el in zip(lat, lon, elev):
-        fg.add_child(folium.Marker(location = [lt, ln], popup = str(el) + " m"))
-    
+        fg.add_child(folium.CircleMarker(location = [lt, ln], radius = 7, popup = str(el) + " m",
+        fill_color = colorCat(el), color = 'grey', fill_opacity = 0.7)) 
+        
+    fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(), 
+    style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'yellow'
+    if x['properties']['POP2005'] < 50000000 else 'orange' }))
+
     nybase.add_child(fg)
 
 
