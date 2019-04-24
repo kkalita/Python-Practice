d4dab1c0d88961ac51495899eeb6d0ea93fa0c79
Abhishek Kalita <abhishek.kalita@vanderbilt.edu>
2019-04-23 21:04:18 -0500

    added layer display options

--- WebMapper/map.py
+++ WebMapper/map.py
@@ -16,17 +16,22 @@ def main():
     elev = list(mapdata["ELEV"])
 
     nybase = folium.Map(location = [40.730610, -73.935242], zoom_start = 5, tiles = "Mapbox Bright")
-    fg = folium.FeatureGroup(name = "demoMap")
+    fgv = folium.FeatureGroup(name = "Volcano")
 
     for lt, ln, el in zip(lat, lon, elev):
-        fg.add_child(folium.CircleMarker(location = [lt, ln], radius = 7, popup = str(el) + " m",
+        fgv.add_child(folium.CircleMarker(location = [lt, ln], radius = 7, popup = str(el) + " m",
         fill_color = colorCat(el), color = 'grey', fill_opacity = 0.7)) 
-        
-    fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(), 
+
+    fgp = folium.FeatureGroup(name = "Country Population")
+
+
+    fgp.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(), 
     style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'yellow'
     if x['properties']['POP2005'] < 50000000 else 'orange' }))
 
-    nybase.add_child(fg)
+    nybase.add_child(fgv)
+    nybase.add_child(fgp)
+    nybase.add_child(folium.LayerControl())
 
 
     nybase.save("nyBaseMap.html")
