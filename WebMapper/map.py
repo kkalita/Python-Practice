2d30556f7575b6c2b582ab26773736a47b5c476c
Abhishek Kalita <abhishek.kalita@vanderbilt.edu>
2019-04-23 20:22:55 -0500

    added volcano points and elevation information

--- WebMapper/map.py
+++ WebMapper/map.py
@@ -1,7 +1,22 @@
 import folium
+import pandas
 
 def main():
-    nybase = folium.Map(location = [40.730610, -73.935242])
+
+    mapdata = pandas.read_csv("Volcanoes.txt")
+    lat = list(mapdata["LAT"])
+    lon = list(mapdata["LON"])
+    elev = list(mapdata["ELEV"])
+
+    nybase = folium.Map(location = [40.730610, -73.935242], zoom_start = 5, tiles = "Mapbox Bright")
+    fg = folium.FeatureGroup(name = "demoMap")
+
+    for lt, ln, el in zip(lat, lon, elev):
+        fg.add_child(folium.Marker(location = [lt, ln], popup = str(el) + " m"))
+    
+    nybase.add_child(fg)
+
+
     nybase.save("nyBaseMap.html")
 
 if __name__ == "__main__":
